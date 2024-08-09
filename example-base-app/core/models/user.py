3ee531b4fbb2f7a823a45962d582ebff2e233bb7
nika-doroshkevich <veronika_doroshkevich@mail.ru>
2024-08-09 23:19:05 +0300

    add method for user database

--- example-base-app/core/models/user.py
+++ example-base-app/core/models/user.py
@@ -1,9 +1,19 @@
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+from typing import TYPE_CHECKING
+
+from fastapi_users.db import (
+    SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase,
+)
 
 from .base import Base
 from .mixins.int_id_pk import IntIdPkMixin
 
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
-class User(IntIdPkMixin, Base):
-    username: Mapped[str] = mapped_column(unique=True)
+
+class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, User)
