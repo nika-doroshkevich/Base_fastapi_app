0372abf56e4d01864f9c942ed455bd6ea6b326bf
nika-doroshkevich <veronika_doroshkevich@mail.ru>
2024-08-10 01:16:21 +0300

    create AccessToken config

--- example-base-app/core/config.py
+++ example-base-app/core/config.py
@@ -37,6 +37,12 @@ class DatabaseConfig(BaseModel):
     }
 
 
+class AccessToken(BaseModel):
+    lifetime_seconds: int = 3600
+    reset_password_token_secret: str
+    verification_token_secret: str
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=(".env.template", ".env"),
@@ -47,6 +53,7 @@ class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
+    access_token: AccessToken
 
 
 settings = Settings()
