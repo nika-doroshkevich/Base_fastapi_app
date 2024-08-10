ec91c4421fab5a6a06bd8a76ca3d33113ec46a04
nika-doroshkevich <veronika_doroshkevich@mail.ru>
2024-08-11 00:33:38 +0300

    create messages router

--- example-base-app/api/api_v1/__init__.py
+++ example-base-app/api/api_v1/__init__.py
@@ -6,6 +6,7 @@ from fastapi.security import HTTPBearer
 
 from core.config import settings
 from .auth import router as auth_router
+from .messages import router as messages_router
 from .users import router as users_router
 
 http_bearer = HTTPBearer(auto_error=False)
@@ -16,3 +17,4 @@ router = APIRouter(
 )
 router.include_router(auth_router)
 router.include_router(users_router)
+router.include_router(messages_router)
