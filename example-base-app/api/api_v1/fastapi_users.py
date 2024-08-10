99cca69d00cd701a079498db98ce848ab0918f5b
nika-doroshkevich <veronika_doroshkevich@mail.ru>
2024-08-11 01:07:41 +0300

    update imports

--- example-base-app/api/api_v1/fastapi_users.py
+++ example-base-app/api/api_v1/fastapi_users.py
@@ -1,7 +1,7 @@
 from fastapi_users import FastAPIUsers
 
-from api.dependencies.authentication.backend import authentication_backend
-from api.dependencies.authentication.user_manager import get_user_manager
+from api.dependencies.authentication import authentication_backend
+from api.dependencies.authentication import get_user_manager
 from core.models import User
 from core.types.user_id import UserIdType
 
