5b547764cb83e099b9b8756d4aa7eff72c475583
nika-doroshkevich <veronika_doroshkevich@mail.ru>
2024-08-11 00:44:15 +0300

    create fastapi-users dependencies for users

--- example-base-app/api/api_v1/fastapi_users.py
+++ example-base-app/api/api_v1/fastapi_users.py
@@ -1,12 +1,14 @@
 from fastapi_users import FastAPIUsers
 
-from core.models import User
-from core.types.user_id import UserIdType
-
 from api.dependencies.authentication.backend import authentication_backend
 from api.dependencies.authentication.user_manager import get_user_manager
+from core.models import User
+from core.types.user_id import UserIdType
 
 fastapi_users = FastAPIUsers[User, UserIdType](
     get_user_manager,
     [authentication_backend],
 )
+
+current_active_user = fastapi_users.current_user(active=True)
+current_active_superuser = fastapi_users.current_user(active=True, superuser=True)
