3fa7acbd86848cd67287f6f30848553bffaf9ca3
nika-doroshkevich <veronika_doroshkevich@mail.ru>
2024-08-12 23:57:05 +0300

    create student grade model

--- example-base-app/core/models/__init__.py
+++ example-base-app/core/models/__init__.py
@@ -4,10 +4,12 @@ __all__ = (
     "User",
     "AccessToken",
     "Student",
+    "StudentGrade",
 )
 
 from .access_token import AccessToken
 from .base import Base
 from .db_helper import db_helper
 from .student import Student
+from .student_grade import StudentGrade
 from .user import User
