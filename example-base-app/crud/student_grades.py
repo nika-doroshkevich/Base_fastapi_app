0dd7cc0587c9a895b8c1d0287bd111cd26e03782
nika-doroshkevich <veronika_doroshkevich@mail.ru>
2024-08-13 01:00:51 +0300

    add ability to get min grade

--- example-base-app/crud/student_grades.py
+++ example-base-app/crud/student_grades.py
@@ -1,3 +1,5 @@
+from sqlalchemy import func
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import StudentGrade
@@ -11,3 +13,14 @@ async def create_student_grade(
     session.add(student_grade)
     await session.commit()
     return student_grade
+
+
+async def get_students_with_min_grade(session: AsyncSession) -> list[StudentGrade]:
+    stmt = select(func.min(StudentGrade.grade))
+    result = await session.execute(stmt)
+    min_grade = result.scalar_one()
+
+    stmt_min = select(StudentGrade).where(StudentGrade.grade == min_grade)
+    students_min = await session.scalars(stmt_min)
+
+    return list(students_min)
